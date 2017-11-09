9d9f28dea2b66f739b8d5f0d42a8ab95b3ea2a0d
HuanMeng <371034309@qq.com>
2017-11-09 11:11:36 +0800

    Update header.php

--- app/views/public/header.php
+++ app/views/public/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>M Framework</title>
+        <title>Magic Framework</title>
         <?php $this->load_css("/static/css/front-end.css"); ?>
     </head>
     <body id="app">
