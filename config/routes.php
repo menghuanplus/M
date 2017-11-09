63f1406be9ab3b9f77581472c68f01c1cb6f8efa
孟焕 <371034309@qq.com>
2017-11-09 09:57:19 +0800

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -6,8 +6,8 @@ use NoahBuscher\Macaw\Macaw;
  * 配置新路由需要在命令行执行 composer dump-autoload
  */
 
-Macaw::get('fuck', function() {
-  echo "成功！";
+Macaw::get('test', function() {
+  echo "路由成功！";
 });
 
 Macaw::get('', 'HomeController@home');
