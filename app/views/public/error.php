a165edd0d36ab85ef9ff282ffbae3215aa9671df
孟焕 <371034309@qq.com>
2017-11-09 10:07:40 +0800

    Update error.php

--- app/views/public/error.php
+++ app/views/public/error.php
@@ -1,6 +1,6 @@
 <?php $this->render("header"); ?>
 
-<div style="text-align: center;line-height: 40px;margin-top:200px;font-size:40px;color:#ccc;">
+<div style="text-align: center;line-height: 40px;margin-top:200px;font-size:200px;color:#ccc;">
 	<p>404</p>
 </div>
 
